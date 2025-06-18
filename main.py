1eca909d47a5d9f7baf54b4c59e4e31ecf4d7b82
VISHNURAM K <132353645+VISHNURAMK098@users.noreply.github.com>
2025-06-18 06:57:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,59 @@ from src.data_loader import load_csv_from_s3
 from src.preprocess import preprocess_data
 from src.train import train_model
 from src.logger import get_logger
+import pandas as pd
+import boto3
+import json
+import os
+import io
 
-def main():
-    logger = get_logger("fraud_detection")
+logger = get_logger("fraud_detection")
 
-    bucket_name = "lambda-code-bucket-ddd"
-    object_key = "upload/transaction_samples.csv"
-
+def run_pipeline():
     logger.info("Starting fraud detection pipeline...")
 
-    df = load_csv_from_s3(bucket_name, object_key)
+    # Load data (local or from S3)
+    s3 = boto3.client("s3")
+    obj = s3.get_object(Bucket="##add your bucket name", Key="upload/transaction_samples.csv")
+    df = pd.read_csv(io.BytesIO(obj["Body"].read()))
     logger.info("Data loaded from S3.")
 
+    # Preprocess
     X_train, X_test, y_train, y_test, scaler = preprocess_data(df)
     logger.info("Data preprocessing complete.")
 
-    train_model(X_train, X_test, y_train, y_test, scaler, logger)
+    # Train and log
+    metrics = train_model(X_train, X_test, y_train, y_test, scaler, logger)
 
-    logger.info("Pipeline finished successfully.")
+    # Save metrics to S3
+    s3 = boto3.client("s3")
+    s3.put_object(
+        Bucket="your-bucket-name",
+        Key="metrics/metrics.json",
+        Body=json.dumps(metrics),
+        ContentType="application/json"
+    )
+    logger.info("Metrics uploaded to S3.")
+
+    return metrics
+
+def lambda_handler(event, context): # GET /?key=metrics/metrics.json
+    try:
+        s3 = boto3.client("s3")
+        obj = s3.get_object(Bucket="your-bucket-name", Key="metrics/metrics.json")
+        metrics = json.loads(obj["Body"].read().decode("utf-8"))
+        return {
+            "statusCode": 200,
+            "body": json.dumps(metrics),
+            "headers": {"Content-Type": "application/json"}
+        }
+    except Exception as e:
+        logger.error("Error in lambda_handler", exc_info=True)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)}),
+            "headers": {"Content-Type": "application/json"}
+        }
 
 if __name__ == "__main__":
-    main()
+    run_pipeline()
