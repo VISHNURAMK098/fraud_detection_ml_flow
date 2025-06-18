c3bd708ae10de834eebdb6c5c9fb48ad0b96e6e7
VISHNURAM K <132353645+VISHNURAMK098@users.noreply.github.com>
2025-06-18 06:44:02 +0000

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,35 +1,33 @@
 import mlflow
 import mlflow.sklearn
-import joblib
-import os
 from sklearn.linear_model import LogisticRegression
 from src.evaluate import evaluate_model
 
-def train_model(X_train, X_test, y_train, y_test, scaler, logger, model_output_dir="models/"):
-    os.makedirs(model_output_dir, exist_ok=True)
-
+def train_model(X_train, X_test, y_train, y_test, scaler, logger):
     with mlflow.start_run():
+
+        logger.info("Starting model training...")
 
         model = LogisticRegression()
         model.fit(X_train, y_train)
-
-        logger.info("Model training complete.")
+        logger.info("Model training completed.")
 
         # Evaluate
         metrics = evaluate_model(model, X_test, y_test, logger)
+        logger.info("Model evaluation completed.")
 
-        # Save locally
-        model_path = os.path.join(model_output_dir, "fraud_model.pkl")
-        scaler_path = os.path.join(model_output_dir, "scaler.pkl")
-        joblib.dump(model, model_path)
-        joblib.dump(scaler, scaler_path)
-        logger.info("Model and scaler saved to disk.")
+        # Log model
+        mlflow.sklearn.log_model(model, artifact_path="sklearn-model")
+        logger.info("Model logged to MLflow.")
 
-        # Log with MLflow
-        mlflow.sklearn.log_model(model, "sklearn-model")
-        mlflow.log_artifact(model_path, artifact_path="model")
-        mlflow.log_artifact(scaler_path, artifact_path="scaler")
-        for k, v in metrics.items():
-            mlflow.log_metric(k, v)
+        # Log metrics
+        for key, value in metrics.items():
+            mlflow.log_metric(key, value)
+            logger.info(f"MLflow metric logged: {key} = {value:.4f}")
 
-        logger.info("Model and scaler logged to MLflow.")
+        # Log scaler params
+        if scaler:
+            mlflow.log_dict(scaler.get_params(), artifact_file="scaler_params.json")
+            logger.info("Scaler parameters logged to MLflow.")
+
+    return metrics
